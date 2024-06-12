1c9bd38e9062167de9f73de9b9cb23182bab0944
emilijamedic <gargamel998@gmail.com>
2024-06-12 17:32:38 +0200

    init

---
+++ CaptainBlaster/Assets/Scripts/GameManager.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine;
+using UnityEngine.UI; // Note this new line is needed for UI
+public class GameManager : MonoBehaviour
+{
+public Text scoreText;
+public Text gameOverText;
+int playerScore = 0;
+public void AddScore()
+{
+playerScore++;
+// This converts the score (a number) into a string
+scoreText.text = playerScore.ToString();
+}
+public void PlayerDied()
+{
+gameOverText.enabled = true;
+// This freezes the game
+Time.timeScale = 0;
+}
+}
